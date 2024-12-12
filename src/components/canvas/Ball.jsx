78e936382cda03c0880e73f772c061bc71889a19
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-12-12 09:07:40 +0545

    Tech, Ball remain but Work done

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+// import React from "react";
+// import { Suspense } from "react";
+// import {
+//   Decal,
+//   Float,
+//   OrbitControls,
+//   Preload,
+//   useTexture,
+// } from "@react-three/drei";
+
+// import { Canvas } from "@react-three/fiber";
+// import CanvasLoader from "../Loader";
+
+// const Ball = (props) => {
+//   const [decal] = useTexture([props.imgUrl]);
+
+//   return (
+//     <>
+//       <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+//         <ambientLight intensity={0.25} />
+//         <directionalLight position={[0, 0, 0.05]} />
+//       </Float>
+
+//       <mesh castShadow receiveShadow scale={2.75}>
+//         <icosahedronBufferGeometry args={[1, 1]} />
+//         <meshStandardMaterial
+//           color="#fff8eb"
+//           polygonOffset
+//           polygonOffsetFactor={-5}
+//           flatShading
+//         />
+//         <Decal
+//           position={[0, 0, 1]}
+//           rotation={[2 * Math.PI]}
+//           flatShading
+//           map={decal}
+//         />
+//       </mesh>
+//     </>
+//   );
+// };
+
+// const BallCanvas = ({ icon }) => {
+//   return (
+//     <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls enableZoom={false} />
+//         <Ball imgUrl={icon} />
+//       </Suspense>
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default BallCanvas;
+
+import React from "react";
 
 const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
-}
+  return <div>Ball</div>;
+};
 
-export default Ball
+export default Ball;
